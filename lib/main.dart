ca8582cc92e4e444f524b9c07d74d48c24fa3dd0
SlaviXG <78792148+SlaviXG@users.noreply.github.com>
2024-06-10 02:19:16 +0300

    fix: loading .env

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:path/path.dart' as path;
 import 'dart:io';
 import 'package:flowlinkapp/screens/home_screen.dart';
+import 'package:flowlinkapp/utils/dotenv.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Construct the file path for the .env file in the new location
+  // Construct the file path for the .env file
   String envFilePath = path.join(Directory.current.path, '.env');
 
   // Debug: Print the current directory and file path
@@ -19,7 +19,7 @@ Future<void> main() async {
     print('.env file found at $envFilePath');
     // Load the .env file
     try {
-      await dotenv.load(fileName: envFilePath);
+      await DotenvUtils.loadEnv();
       print('.env file loaded successfully');
     } catch (e) {
       print('Failed to load .env file: $e');
